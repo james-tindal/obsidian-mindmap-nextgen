f4249083ff05aa3565e36007e9283d735a089813
MasterTuto <brenocarvalho2011@gmail.com>
2022-12-04 01:06:03 -0300

    feat: add default screenshot bg color to settings

--- src/settings.ts
+++ src/settings.ts
@@ -8,18 +8,21 @@ export class MindMapSettings {
   spacingHorizontal: number = 80;
   paddingX: number = 8;
   color1: string = "#fed766";
-  color1Thickness: string = '8';
+  color1Thickness: string = "8";
 
   color2: string = "#2ab7ca";
-  color2Thickness: string = '6';
+  color2Thickness: string = "6";
 
   color3: string = "#fe4a49";
-  color3Thickness: string = '4';
+  color3Thickness: string = "4";
 
   defaultColor: string = "#000";
-  defaultColorThickness: string = '2';
+  defaultColorThickness: string = "2";
 
   initialExpandLevel: number = 1;
 
   onlyUseDefaultColor = false;
+
+  // below a beautiful dark blue color
+  screenshotBgColor: string = "#0d1147";
 }
