aa68bc11f5c79cb3d13784b082c405aec1a7cbef
MasterTuto <brenocarvalho2011@gmail.com>
2022-12-13 09:26:30 -0300

    fix: change checkboxes

--- src/custom-plugins.ts
+++ src/custom-plugins.ts
@@ -48,39 +48,54 @@ export const checkBoxPlugin: ITransformPlugin = {
     },
   },
   transform: (transformHooks) => {
-    transformHooks.beforeParse.tap((md, context) => {
-      md.parse = wrapFunction(md.parse, {
-        after: function (ctx) {
-          const replaceSingle = (token: Token) => {
-            const active = [
-              ["[ ] ", "⬜ "],
-              ["[x] ", "✅ "],
-              ["[X] ", "✅ "],
+    transformHooks.parser.tap((md) => {
+      md.use((md, options) => {
+        md.inline.ruler.push(
+          "checkbox",
+          (state, silent) => {
+            const mdCheckRegex = /^ *-? *\[[ xX]\] +/;
 
-              ["- [ ] ", "⬜ "],
-              ["- [x] ", "✅ "],
-              ["- [X] ", "✅ "],
-            ].find(([a, b]) => token.content?.startsWith(a));
-
-            if (active) {
-              token.content = token.content?.replace(active[0], active[1]);
+            if (!mdCheckRegex.test(state.src)) {
+              return false;
             }
 
-            return token;
-          };
+            const mdCheckedRegex = /^ *-? *\[[xX]\] +/;
+            const mdUncheckedRegex = /^ *-? *\[ \] +/;
 
-          const replaceSymbols = (token: Token) => {
-            if (token.children)
-              token.children = token.children.map(replaceSingle);
+            if (mdCheckedRegex.test(state.src)) {
+              state.src = state.src.replace(mdCheckedRegex, "");
+              state.push({
+                type: "checkbox_chkd",
+                content: "checkbox_chkd",
+                level: state.level + 1,
+              });
+            } else if (mdUncheckedRegex.test(state.src)) {
+              state.src = state.src.replace(mdUncheckedRegex, "");
+              state.push({
+                type: "checkbox_unchkd",
+                content: "checkbox_unchkd",
+                level: state.level + 1,
+              });
+            }
 
-            return token;
-          };
-
-          ctx.result = ctx.result.map(replaceSymbols);
-        },
+            return true;
+          },
+          {}
+        );
       });
+
+      md.renderer.rules.checkbox_chkd = () => {
+        return `<span style="color: green; font-weight: bold;">✓</span>`;
+      };
+
+      md.renderer.rules.checkbox_unchkd = () => {
+        return `<span style="color: red; font-weight: bold;">✗</span>`;
+      };
     });
 
-    return { styles: void 0, scripts: void 0 };
+    return {
+      styles: void 0,
+      scripts: void 0,
+    };
   },
 };
