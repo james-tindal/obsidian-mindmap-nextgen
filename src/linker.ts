e03e5df1690028775ac06834a6e827c27c8fab7e
james-tindal <10291002+james-tindal@users.noreply.github.com>
2023-01-24 19:10:45 +0000

    refactor: linker.ts is a module, not a class

--- src/linker.ts
+++ src/linker.ts
@@ -3,46 +3,40 @@ import { getLinkpath } from "obsidian";
 
 import { INTERNAL_LINK_REGEX } from "./constants";
 
-export default class Linker {
-  vaultName: string;
-
-  constructor() {
-    this.vaultName = app.vault.getName();
-  }
-
-  updateInternalLinks(node: INode) {
-    this.replaceInternalLinks(node);
-    if (node.children) {
-      node.children.forEach((n) => this.updateInternalLinks(n));
-    }
-  }
-
-  private replaceInternalLinks(node: INode) {
-    const matches = this.parseValue(node.content);
-    for (let i = 0; i < matches.length; i++) {
-      const match = matches[i];
-      const isWikiLink = match.groups["wikitext"];
-      const linkText = isWikiLink
-        ? match.groups["wikitext"]
-        : match.groups["mdtext"];
-      const linkPath = isWikiLink ? linkText : match.groups["mdpath"];
-      if (linkPath.startsWith("http")) {
-        continue;
-      }
-      const url = `obsidian://open?vault=${this.vaultName}&file=${
-        isWikiLink ? encodeURI(getLinkpath(linkPath)) : linkPath
-      }`;
-      const link = `<a href=\"${url}\">${linkText}</a>`;
-      node.content = node.content.replace(match[0], link);
-    }
-  }
-
-  private parseValue(v: string) {
-    const matches = [];
-    let match;
-    while ((match = INTERNAL_LINK_REGEX.exec(v))) {
-      matches.push(match);
-    }
-    return matches;
+export function updateInternalLinks(node: INode) {
+  replaceInternalLinks(node);
+  if (node.children) {
+    node.children.forEach(updateInternalLinks);
   }
 }
+
+function replaceInternalLinks(node: INode) {
+  const matches = parseValue(node.content);
+  for (let i = 0; i < matches.length; i++) {
+    const match = matches[i];
+    const isWikiLink = match.groups["wikitext"];
+    const linkText = isWikiLink
+      ? match.groups["wikitext"]
+      : match.groups["mdtext"];
+    const linkPath = isWikiLink ? linkText : match.groups["mdpath"];
+    if (linkPath.startsWith("http")) {
+      continue;
+    }
+    const vaultName = app.vault.getName();
+    const url = `obsidian://open?vault=${vaultName}&file=${
+      isWikiLink ? encodeURI(getLinkpath(linkPath)) : linkPath
+    }`;
+    const link = `<a href=\"${url}\">${linkText}</a>`;
+    node.content = node.content.replace(match[0], link);
+  }
+}
+
+function parseValue(v: string) {
+  const matches = [];
+  let match;
+  while ((match = INTERNAL_LINK_REGEX.exec(v))) {
+    matches.push(match);
+  }
+  return matches;
+}
+
