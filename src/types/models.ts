b50027ab3f18bdb33490a0167a469ad36d5a3040
james-tindal <10291002+james-tindal@users.noreply.github.com>
2023-02-10 01:47:17 +0000

    refactor: tidy render function

--- src/types/models.ts
+++ src/types/models.ts
@@ -1,12 +1,12 @@
 type IMarkmapOptions = import("markmap-common").IMarkmapOptions;
 type IMarkmapJSONOptions = import("markmap-common").IMarkmapJSONOptions;
 
-export type FrontmatterOptions = Partial<IMarkmapOptions> & {
-  screenshotTextColor?: string;
+export type FrontmatterOptions = Partial<IMarkmapOptions & {
+  screenshotTextColor: string;
   screenshotBgColor: string;
-  highlight?: boolean;
+  highlight: boolean;
   titleAsRootNode: boolean;
-};
+}>;
 
 export type TokenWithChildren = Remarkable.Remarkable.Token & {
   content?: string;
