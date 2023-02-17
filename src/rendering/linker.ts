a2ddeb8d818bd9374181b0ec8bce16b3e82ba084
londonsangongzi <londonsangongzi@hotmail.com>
2023-02-17 15:29:57 +0000

    fix: internal links break with &, | chars

--- src/rendering/linker.ts
+++ src/rendering/linker.ts
@@ -15,17 +15,31 @@ function replaceInternalLinks(node: INode) {
   for (let i = 0; i < matches.length; i++) {
     const match = matches[i];
     const isWikiLink = match.groups!["wikitext"];
-    const linkText = isWikiLink
+    let linkText = isWikiLink
       ? match.groups!["wikitext"]
       : match.groups!["mdtext"];
-    const linkPath = isWikiLink ? linkText : match.groups!["mdpath"];
+    let linkPath = isWikiLink ? linkText : match.groups!["mdpath"];
     if (linkPath.startsWith("http")) {
       continue;
     }
     const vaultName = app.vault.getName();
-    const url = `obsidian://open?vault=${vaultName}&file=${
-      isWikiLink ? encodeURI(getLinkpath(linkPath)) : linkPath
-    }`;
+    let url = "";
+    if (isWikiLink) {
+      // "&": [[A & B]]
+      linkPath = linkPath.replace(/&amp;/g, "&");
+      // "|": [[A & B|AB]]
+      const regex = /^(.*?)\|(.*)$/;
+      const match_link_text = regex.exec(linkPath);
+      if(match_link_text && match_link_text.length === 3){
+        linkText = match_link_text[2];
+        linkPath = match_link_text[1].trim();
+      }
+      url = `obsidian://open?vault=${vaultName}&file=${encodeURIComponent(getLinkpath(linkPath))}`;
+    } else {
+      // "&": [AB](<A & B.md>)
+      linkPath = linkPath.replace(/&amp;/g, "%26");
+      url = `obsidian://open?vault=${vaultName}&file=${linkPath}`;
+    }    
     const link = `<a href=\"${url}\">${linkText}</a>`;
     node.content = node.content.replace(match[0], link);
   }
