781605f2f4dccee8394aa680416cfe05f4c49f9b
james-tindal <10291002+james-tindal@users.noreply.github.com>
2025-01-23 00:03:30 +0000

    feat: transform internal links in mindmap

--- src/rendering/internal-links.ts
+++ src/rendering/internal-links.ts
@@ -1,5 +1,4 @@
 import { INode } from 'markmap-common'
-import { getLinkpath } from 'obsidian'
 
 
 export function updateInternalLinks(node: INode) {
@@ -12,43 +11,16 @@ export function updateInternalLinks(node: INode) {
 function replaceInternalLinks(node: INode) {
   const matches = matchRegex(node.content)
   for (const match of matches) {
-    const isWikiLink = match.groups!['wikitext']
-    let linkText = isWikiLink
-      ? match.groups!['wikitext']
-      : match.groups!['mdtext']
-    let linkPath = isWikiLink ? linkText : match.groups!['mdpath']
-    if (linkPath.startsWith('http')) {
-      continue
-    }
-    const vaultName = app.vault.getName()
-    let url = ''
-    if (isWikiLink) {
-      // "&": [[A & B]]
-      linkPath = linkPath.replace(/&amp;/g, '&')
-      // "|": [[A & B|AB]]
-      const regex = /^(.*?)\|(.*)$/
-      const match_link_text = regex.exec(linkPath)
-      if (match_link_text && match_link_text.length === 3) {
-        linkText = match_link_text[2]
-        linkPath = match_link_text[1].trim()
-      }
-      url = `obsidian://open?vault=${vaultName}&file=${encodeURIComponent(getLinkpath(linkPath))}`
-    } else {
-      // "&": [AB](<A & B.md>)
-      linkPath = linkPath.replace(/&amp;/g, '%26')
-      url = `obsidian://open?vault=${vaultName}&file=${linkPath}`
-    }    
-    const link = `<a href=\"${url}\">${linkText}</a>`
-    node.content = node.content.replace(match[0], link)
+    const { link, displayText } = match.groups!
+    const html = `<a href=\"${link}\">${displayText || link}</a>`
+    node.content = node.content.replace(match[0], html)
   }
 }
 
-// https://regex101.com/r/gw85cc/2
-const regex = /\[\[(?<wikitext>.*)\]\]|<a href="(?<mdpath>.*)">(?<mdtext>.*)<\/a>/gim
-
+const wikilinkRegex = /\[\[(?<link>[^|\]]+)\|?((?<displayText>.+))?\]\]/g
 function* matchRegex(str: string) {
   while (true) {
-    const match = regex.exec(str)
+    const match = wikilinkRegex.exec(str)
     if (!match) break
     yield match
   }
