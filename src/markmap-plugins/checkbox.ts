9fa1831ea898fb0a74c3b177c90c6e2cb0d58750
james-tindal <10291002+james-tindal@users.noreply.github.com>
2025-01-24 20:00:49 +0000

    refactor: markmap plugin: checkbox

--- src/markmap-plugins/checkbox.ts
+++ src/markmap-plugins/checkbox.ts
@@ -1,47 +1,46 @@
 import { ITransformPlugin } from 'markmap-lib'
+import { InlineParsingRule } from 'remarkable/lib'
 
 /*
  *  Display Obsidian checkboxes in the mindmap
  *  https://github.com/james-tindal/obsidian-mindmap-nextgen#checkboxes
  */
+
+const parser: InlineParsingRule = state => {
+  if (state.pos !== 0) return false
+
+  const match = /^ *-? *\[(?<state>[ xX])\] +/.exec(state.src)
+  const checked = match?.groups?.state !== ' '
+  const length = match?.[0].length!
+  const token = `checkbox_${checked ? '' : 'un'}chkd`
+
+  if (!match) return false
+
+  state.push({
+    type: token,
+    level: state.level,
+    block: false
+  })
+
+  state.pos += length
+  return true
+}
+
 export const checkBoxPlugin: ITransformPlugin = {
   name: 'checkbox',
   config: {
     version: {
-      checkbox: '1.0',
-    },
+      checkbox: '1.0'
+    }
   },
   transform: transformHooks => {
     transformHooks.parser.tap(md => {
-      md.use(md => {
-        md.inline.ruler.push('checkbox', state => {
-          if (state.pos !== 0) return false
-
-          const match = /^ *-? *\[(?<state>[ xX])\] +/.exec(state.src)
-          const checked = match?.groups?.state !== ' '
-          const length = match?.[0].length!
-          const token = `checkbox_${checked ? '' : 'un'}chkd`
-
-          if (!match) return false
-
-          state.push({
-            type: token,
-            level: state.level,
-            block: false
-          })
-
-          state.pos += length
-          return true
-        }, {})
-      })
+      md.inline.ruler.push('checkbox', parser, {})
 
       md.renderer.rules.checkbox_chkd   = () => '<span class="mm-ng-checkbox-checked">✓&nbsp;</span>'
       md.renderer.rules.checkbox_unchkd = () => '<span class="mm-ng-checkbox-unchecked">✗&nbsp;</span>'
     })
 
-    return {
-      styles: void 0,
-      scripts: void 0,
-    }
-  },
+    return {}
+  }
 }
