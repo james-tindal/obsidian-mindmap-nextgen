1888c689a6fdc718de4aff42386ca8a4e6d6e84e
james-tindal <10291002+james-tindal@users.noreply.github.com>
2025-02-04 16:15:26 +0000

    fix: no need to remove unrecognised language tags with highlightjs

--- src/rendering/renderer-common.ts
+++ src/rendering/renderer-common.ts
@@ -13,29 +13,15 @@ export const transformer = new Transformer([ ...builtInPlugins, checkBoxPlugin ]
 export function parseMarkdown<Type extends 'file' | 'codeBlock'>(text: string) {
   ;(GrayMatter as typeof GrayMatter & { clearCache: Function }).clearCache()
   const gm = GrayMatter(text)
-  const content = removeUnrecognisedLanguageTags(gm.content)
   const frontmatter = gm.data
   
-  const { root, features } = transformer.transform(content)
+  const { root, features } = transformer.transform(gm.content)
   loadAssets(features)
   parseInternalLinks(root)
 
   const settings = (frontmatter?.markmap || {}) as Type extends 'file' ? FileSettings : CodeBlockSettings
 
   return { rootNode: root, settings, body: gm.content }
-}
-
-export function removeUnrecognisedLanguageTags(markdown: string) {
-  // Remove info string from code fence unless it in the list of default languages from
-  // https://prismjs.com/#supported-languages
-  const allowedLanguages = ['markup', 'html', 'xml', 'svg', 'mathml', 'ssml', 'atom', 'rss', 'js', 'javascript', 'css', 'clike']
-  return markdown.replace(/```(.+)/g, (_, capture) => {
-    const backticks = capture.match(/(`*).*/)?.[1]
-    const infoString = capture.match(/`*(.*)/)?.[1]
-    const t = infoString?.trim()
-    const sanitisedInfoString = allowedLanguages.includes(t) ? t : ''
-    return '```' + (backticks || '') + sanitisedInfoString
-  })
 }
 
 export function loadAssets(features: IFeatures) {
