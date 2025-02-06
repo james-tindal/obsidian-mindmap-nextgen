8069476bd0deccbd84323de79bd973b582d37d39
james-tindal <10291002+james-tindal@users.noreply.github.com>
2025-02-06 13:30:02 +0000

    feat: page previews

--- src/internal-links/catch-internal-links.ts
+++ src/internal-links/catch-internal-links.ts
@@ -4,19 +4,44 @@ import { pluginState } from 'src/core/state'
 
 const workspace = document.querySelector('.workspace' as 'div')!
 
-plugin.registerDomEvent(workspace, 'click', event => {
-  const { target } = event
-  const isAnchor = target instanceof HTMLAnchorElement
-    if (!isAnchor) return
-  const parentMindmap = target.closest('svg.markmap' as 'svg')
-    if (!parentMindmap) return
-  const href = target.getAttribute('href')
-    if (!href) return
-  const hasProtocol = /^[^#:]*:.*#/.test(href)  // https://regex101.com/r/RLG0a5/1
-    if (hasProtocol) return
+const registerEvent = (type: 'click' | 'mouseover', fn: (args: {
+  event: MouseEvent
+  target: HTMLAnchorElement
+  parentMindmap: SVGSVGElement
+  href: string
+}) => void) =>
+  plugin.registerDomEvent(workspace, type, event => {
+    const { target } = event
+    const isAnchor = target instanceof HTMLAnchorElement
+      if (!isAnchor) return
+    const parentMindmap = target.closest('svg.markmap' as 'svg')
+      if (!parentMindmap) return
+    const href = target.getAttribute('href')
+      if (!href) return
+    const hasProtocol = /^[^#:]*:.*#/.test(href)  // https://regex101.com/r/RLG0a5/1
+      if (hasProtocol) return
 
+    fn({ event, target, parentMindmap, href })
+  })
+
+registerEvent('click', ({ event, parentMindmap, href }) => {
   event.preventDefault()
 
   const { path } = pluginState.svgs.get(parentMindmap)!
   app.workspace.openLinkText(href, path)
+})
+
+plugin.registerHoverLinkSource('mindmap-nextgen', {
+  display: 'Mindmap Nextgen',
+  defaultMod: true
+})
+
+registerEvent('mouseover', ({ event, target, parentMindmap, href }) => {
+  app.workspace.trigger('hover-link', {
+    event,
+    source: 'mindmap-nextgen',
+    hoverParent: parentMindmap,
+    targetEl: target,
+    linktext: href
+  })
 })
