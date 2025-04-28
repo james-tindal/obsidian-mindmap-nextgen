59bcbb854e2ef9ac9852bc4f918882548c16f8eb
james-tindal <10291002+james-tindal@users.noreply.github.com>
2025-04-28 17:23:33 +0100

    refactor: add utility: callbag from obsidian event

---
+++ src/utilities/from-obsidian-event.ts
@@ -0,0 +1,57 @@
+import type { Events } from 'obsidian'
+import Callbag from './callbag'
+import { plugin } from 'src/core/entry'
+
+
+// Typing could fail if target has more than 20 events
+export function fromObsidianEvent<
+  Target extends Events,
+  Name extends GetName<Target>
+>(target: Target, name: Name) {
+  type OnMethod = GetOnMethod<Target>
+  type OnMethodParams = OverloadedParams<OnMethod>
+  type Callback = GetCallback<OnMethodParams, Name>
+  type CallbackParams = Parameters<Callback>
+
+  return Callbag.create<CallbackParams>((next, error, complete) => {
+    const sendArgsArray = (...args: CallbackParams) => next(args)
+    const ref = target.on(name, sendArgsArray)
+    plugin.registerEvent(ref)
+    plugin.register(complete)
+  })
+}
+  
+type GetCallback<Params extends [...args: any], Name extends string> =
+  Params extends [name: Name, callback: infer Callback, ...args: any[]]
+    ? Callback : never
+
+type GetOnMethod<Target extends Events> =
+  Target['on']
+
+type GetName<Target extends Events> =
+  OverloadedParams<GetOnMethod<Target>>[0]
+
+type OverloadedParams<T> = T extends {
+  (...args: infer A1): any
+  (...args: infer A2): any
+  (...args: infer A3): any
+  (...args: infer A4): any
+  (...args: infer A5): any
+  (...args: infer A6): any
+  (...args: infer A7): any
+  (...args: infer A8): any
+  (...args: infer A9): any
+  (...args: infer A10): any
+  (...args: infer A11): any
+  (...args: infer A12): any
+  (...args: infer A13): any
+  (...args: infer A14): any
+  (...args: infer A15): any
+  (...args: infer A16): any
+  (...args: infer A17): any
+  (...args: infer A18): any
+  (...args: infer A19): any
+  (...args: infer A20): any
+} ? ( A1 | A2 | A3 | A4 | A5 | A6 | A7 | A8 | A9 | A10
+    | A11 | A12 | A13 | A14 | A15 | A16 | A17 | A18 | A19 | A20
+) : never
