65f45c9f5cb528d15c72b494fe611f6a1224fa55
james-tindal <10291002+james-tindal@users.noreply.github.com>
2025-09-29 16:40:15 +0100

    add callbag groupBy method

---
+++ src/utilities/callbag/group-by.ts
@@ -0,0 +1,44 @@
+import create from 'callbag-create'
+import { Source } from 'callbag'
+import Callbag, { Subject, subject } from '.'
+import { Merge } from 'type-fest'
+
+
+export type GroupedSource<Key, T> = Source<T> & { key: Key }
+export type GroupedSubject<Key, T> = Merge<Subject<T>, { source: GroupedSource<Key, T> }>
+export const groupBy = <Key, In>(keyFn: (data: In) => Key) => (source: Source<In>): Source<GroupedSource<Key, In>> => 
+  create((next, error, complete) => {
+    const groups = new Map<Key, GroupedSubject<Key, In>>()
+    return Callbag.subscribe(source, {
+      error(reason) {
+        error(reason)
+        for (const { push } of groups.values())
+          push.error(reason)
+      },
+      complete() {
+        complete()
+        for (const { push } of groups.values())
+          push.complete()
+      },
+      next(data) {
+        const key = keyFn(data)
+        if (groups.has(key))
+          groups.get(key)!.push(data)
+        else {
+          const sub = groupedSubject<Key, In>(key)
+          groups.set(key, sub)
+          next(sub.source)
+          sub.push(data)
+        }
+      }
+    })
+  }
+)
+
+const groupedSubject = <Key, T>(key: Key): GroupedSubject<Key, T> => {
+  const sub = subject<T>()
+  return {
+    source: Object.assign(sub.source, { key }),
+    push: sub.push
+  }
+}
